7dd5bd048c9dd98fda9db8dacaf3672f06d517d5
Oleg Kosharnyi <kosarnijoleg@gmail.com>
2026-04-26 13:16:56 +0300

    Add homework 15.1

---
+++ hillel_15_1/script.js
@@ -0,0 +1,70 @@
+const form = document.querySelector('.js--form');
+const input = document.querySelector('.js--form__input');
+const todosWrapper = document.querySelector('.js--todos-wrapper');
+
+
+let todos = JSON.parse(localStorage.getItem('todos')) || [];
+
+
+function render() {
+    todosWrapper.innerHTML = ''; 
+
+    todos.forEach((todo, index) => {
+        const li = document.createElement('li');
+        li.className = `todo-item ${todo.checked ? 'todo-item--checked' : ''}`;
+        li.dataset.index = index; 
+
+        li.innerHTML = `
+            <input type="checkbox" class="js--todo-check" ${todo.checked ? 'checked' : ''}>
+            <span class="todo-item__description">${todo.text}</span>
+            <button class="todo-item__delete js--todo-delete">Видалити</button>
+        `;
+
+        todosWrapper.appendChild(li);
+    });
+}
+
+
+function save() {
+    localStorage.setItem('todos', JSON.stringify(todos));
+}
+
+// 5. Добавление новой задачи
+form.addEventListener('submit', (e) => {
+    e.preventDefault();
+
+    const text = input.value.trim();
+    if (text) {
+        todos.push({
+            text: text,
+            checked: false
+        });
+        input.value = ''; 
+        save();
+        render();
+    }
+});
+
+todosWrapper.addEventListener('click', (e) => {
+    const target = e.target;
+    const parentLi = target.closest('.todo-item');
+    if (!parentLi) return;
+
+    const index = parentLi.dataset.index;
+
+    if (target.classList.contains('js--todo-delete')) {
+        todos.splice(index, 1);
+        save();
+        render();
+    }
+
+    
+    if (target.classList.contains('js--todo-check')) {
+        todos[index].checked = target.checked;
+        save();
+        render();
+    }
+});
+
+
+render();
