f40f211ef42e5750b41f50c8c9e0a81d63976194
Oleg Kosharnyi <kosarnijoleg@gmail.com>
2026-06-01 12:20:51 +0300

    fix: 19.1 script.js

--- hillel_19_1/js/script.js
+++ hillel_19_1/js/script.js
@@ -1,15 +1,12 @@
-// script.js
 
-// Собираем URL динамически, используя данные из config.js
 const URL = `https://api.openweathermap.org/data/2.5/weather?lat=${WEATHER_CONFIG.LAT}&lon=${WEATHER_CONFIG.LON}&appid=${WEATHER_CONFIG.API_KEY}&units=${WEATHER_CONFIG.UNITS}&lang=${WEATHER_CONFIG.LANG}`;
 
-// Находим элементы в DOM
 const cityElement = document.getElementById('city');
 const tempElement = document.getElementById('temp');
 const descElement = document.getElementById('desc');
 const refreshBtn = document.getElementById('refresh-btn');
 
-// Наша асинхронная функция
+
 async function getWeather() {
     try {
         cityElement.textContent = "Оновлення...";
@@ -31,15 +28,12 @@ async function getWeather() {
     }
 }
 
-// Функция отрисовки данных
 function updateUI(data) {
     cityElement.textContent = data.name; 
     tempElement.textContent = `${Math.round(data.main.temp)}°C`; 
     descElement.textContent = data.weather[0].description; 
 }
 
-// Навешиваем слушатели событий
 refreshBtn.addEventListener('click', getWeather);
 
-// Первый запуск при загрузке страницы
 getWeather();
